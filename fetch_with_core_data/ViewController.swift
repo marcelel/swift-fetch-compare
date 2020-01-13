5d11578725d8df0bb85d5b6f261b714bb2304741
marcelel <marcelel321@gmail.com>
2020-01-13 23:09:50 +0100

    Initial Commit

---
+++ fetch_with_core_data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fetch_with_core_data
+//
+//  Created by Amelia Lekston on 13/01/2020.
+//  Copyright © 2020 Marcel Lekston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
